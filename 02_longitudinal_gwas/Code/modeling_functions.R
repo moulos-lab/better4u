b34eafef77bfcbb7b4f65ee17bbb1a471bb37795
lamprosbouranis <lampros.bouranis@gmail.com>
2025-05-08 10:59:47 +0300

    Addressing scalability of analysis with irregular measurements.

--- 02_longitudinal_gwas/Code/modeling_functions.R
+++ 02_longitudinal_gwas/Code/modeling_functions.R
@@ -1,20 +1,26 @@
-
-# input_data = sumstats_weight_adulthood
-# colnames(input_data)[1:15]
-
-modeling_fun <- function(input_data,
-                         pheno    = c("BMI", "Weight"),
-                         numcores = 6)
+modeling_fun <- function(pheno_data,
+                         geno_data,
+                         SNP_IDs,
+                         pheno    = c("BMI", "Weight")#,
+                         #numcores = 4
+                         )
 {
   
-  `%ni%` = Negate(`%in%`)
+  # Source:https://privefl.github.io/bigstatsr/articles/bigstatsr-and-bigmemory.html
+  BM2FBM <- function(bm) {
+    FBM(nrow        = nrow(bm), 
+        ncol        = ncol(bm), 
+        type        = typeof(bm),
+        backingfile = file.path(bigmemory::dir.name(bm), bigstatsr::sub_bk(file.name(bm))),
+        create_bk   = FALSE)
+  }
   
   output        <- vector("list", 2)
   names(output) <- c("Effect_sizes", "Variance_ratio")
   
   # For a linux environment:
   # https://stackoverflow.com/questions/62644063/how-to-run-an-lapply-in-parallel-in-r
-  cl            <- makeCluster(numcores, type = "FORK")
+  #cl            <- makeCluster(numcores, type = "FORK")
   
   if (pheno == "BMI"){
     
@@ -32,85 +38,83 @@ modeling_fun <- function(input_data,
     model_covar_nogenos <- paste(c("M_Age*Sex", paste("PC",as.character(1:10), sep = "")), collapse = " + ")
     formula_model0      <- paste(model_phenotype, model_covar_nogenos, sep = " ~ ")
     
-    model0              <- lm(formula_model0, data = input_data)
+    model0              <- lm(formula_model0, data = pheno_data)
     summary_m0          <- summary(model0)
     
-    input_data$Sample_Weight <- (input_data$N-1)*(input_data$S_Age^2) # Interaction of gender with age^2
-    dd              <- as.data.frame(cbind(input_data$Sample_Weight, 
-                                           input_data$Sample_Weight * (model0$residuals^2)))
+    pheno_data$Sample_Weight <- (pheno_data$N-1)*(pheno_data$S_Age^2) # Interaction of gender with age^2
+    dd              <- as.data.frame(cbind(pheno_data$Sample_Weight, 
+                                           pheno_data$Sample_Weight * (model0$residuals^2)))
     colnames(dd)    <- c("w","y")
     model1          <- MASS::rlm(cbind(0*dd$w + 1,dd$w), dd$y)
     summary_m1      <- summary(model1)
     
-    input_data$Sample_Weight <- 1 / ( (1/input_data$Sample_Weight) + summary_m1$coefficients[2] / summary_m1$coefficients[1] )
+    pheno_data$Sample_Weight <- 1 / ( (1/pheno_data$Sample_Weight) + summary_m1$coefficients[2] / summary_m1$coefficients[1] )
     
     output$Variance_ratio <- as.numeric(model1$coefficients[2]/model1$coefficients[1])
     
     cat("> Estimating SNP effects...")
-    remove_cols <- c("IID",
-                     "BMI_Beta", 
-                     "M_Age",
-                     "S_Age",
-                     "N",
-                     "Sample_Weight",
-                     "Sex", 
-                     paste0("PC", 1:10))
-    model_covar_genos <- colnames(input_data)[colnames(input_data) %ni% c(remove_cols)]
     
-    # Takes a few secs:
-    SNP_mapping    <- data.frame(Original_name = model_covar_genos,
-                                 Mapping       = paste0("SNP", 1:length(model_covar_genos)))
+    # Extract phenotype and covariates:
+    pheno_data$M_Age_Sex <- pheno_data$M_Age * pheno_data$Sex # Interaction term for Mid-age and gender
     
-    nonSNP_mapping <- data.frame(Original_name = remove_cols,
-                                 Mapping       = remove_cols)
+    keep_cols_cols <- c("M_Age_Sex",
+                        paste0("PC", 1:10))
     
-    all_mapping    <- rbind(nonSNP_mapping, SNP_mapping)
+    y          <- pheno_data$BMI_Beta
+    covariates <- as.matrix(pheno_data[, keep_cols_cols])
     
-    colnames(input_data) <- all_mapping[match(colnames(input_data), all_mapping[,1]), 2]
+    ###########################
+    # Run per-SNP regression adjusting for age and gender
+    assoc_results <- bigstatsr::big_univLinReg(X      = geno_data, 
+                                               y, 
+                                               covar  = covariates#,
+                                               #ncores = numcores
+                                               )
     
-    #colnames(input_data)[1:20]
-    #colnames(input_data)[(length(colnames(input_data))-5):length(colnames(input_data))]
+    assoc_results$p.value   <- predict(assoc_results, log10 = FALSE)
+    assoc_results$score     <- NULL
+    colnames(assoc_results) <- c("Beta","SE","P")
     
-    fitAllSNPs <-
-      pbapply::pblapply(SNP_mapping$Mapping[1:length(SNP_mapping$Mapping)], 
-                        function(SNP, 
-                                 input_data,
-                                 model_covar_nogenos,
-                                 model_phenotype){
-                          model1_covars     <- paste(model_covar_nogenos, SNP,       sep = " + ") 
-                          formula_model1    <- paste(model_phenotype, model1_covars, sep = " ~ ")
-                          
-                          fit <- lm(formula_model1,
-                                    data    = input_data,
-                                    weights = input_data$Sample_Weight
-                          )
-                          
-                          summary_m2    <- summary(fit)$coefficients
-                          extract_stats <- summary_m2[rownames(summary_m2)[rownames(summary_m2) %in% SNP],]
-                          
-                          # If the SNP for all samples is homozygous (2), or heterozygous (1), 
-                          # or there is no variant detected (0), there is no variability to be
-                          # modelled and the effect estimates are NA. Set them to -9999.
-                          tmp_names <- colnames(extract_stats)
-                          if( length(extract_stats) == 0)  extract_stats <- rbind(extract_stats,
-                                                                                  rep(-9999, 4))
-                          
-                          return(extract_stats)
-                        }, 
-                        input_data          = input_data,
-                        model_covar_nogenos = model_covar_nogenos,
-                        model_phenotype     = model_phenotype,
-                        cl = cl)
+    #attr(assoc_results, "predict")
     
-    out           <- data.frame(do.call(rbind, fitAllSNPs))
-    out$"t.value" <- NULL
+    # dfs                     <- nrow(pheno_data) - length(keep_cols_cols) - 1 # n - k predictors - 1
+    # assoc_results$Pvalue2   <- pchisq((assoc_results[,1]/assoc_results[,2])^2, df=1, lower=F) # Source: https://www.mv.helsinki.fi/home/mjxpirin/GWAS_course/material/GWAS9.html
+    # assoc_results$Pvalue3 <- 2*stats::pt(assoc_results[,1]/assoc_results[,2], 
+    #                                    df         = dfs, 
+    #                                    lower.tail = FALSE, 
+    #                                    log.p      = FALSE) # Source: https://advstats.psychstat.org/book/mregression/index.php
+    # 
+    # head(assoc_results)
+
+   #---- Transform the linear regression coefs and SEs and then calculate the p-value using the X^2 distribution:
+    y_w          <- pheno_data$BMI_Beta * sqrt(pheno_data$Sample_Weight)
+    covariates_w <- sweep(pheno_data[, keep_cols_cols], 1, sqrt(pheno_data$Sample_Weight), "*")
+    covariates_w <- as.matrix(covariates_w)
+    
+    geno_data_w <- geno_data$bm()
+    #class(geno_data_w)
+    #head(geno_data_w[1:3,1:3])
+    
+    # Source: https://privefl.github.io/bigstatsr/articles/bigstatsr-and-bigmemory.html
+    #geno_data_w <- BM2FBM( sweep(geno_data_w[,1:ncol(geno_data_w)], 1, sqrt(pheno_data$Sample_Weight), "*") )
+    #class(geno_data_w)
+    geno_data_w <- BM2FBM(geno_data_w)
+    #class(geno_data_w)
+  
+    out <- bigstatsr::big_univLinReg(X      = geno_data_w, 
+                                     y_w, 
+                                     covar  = covariates_w#,
+                                     #ncores = numcores
+                                     )
+    
+    out$p.value <- predict(out, log10 = FALSE)
+    out$score <- NULL
     colnames(out) <- c("Beta","SE","P")
     
-    # effect_size_output$AF <- mean(input_data$geno)/2 # Anders should explain the rationale for calculating the Allelic Frequency
-    
-    out                 <- cbind(SNP_mapping$Original_name, out)
-    colnames(out)[1]    <- "SNP"
-    output$Effect_sizes <- out
+    rm(assoc_results)
+    # head(out)
+
+    output$Effect_sizes <- cbind(SNP_IDs, out)
     
   } else {
     
@@ -129,94 +133,89 @@ modeling_fun <- function(input_data,
     model_covar_nogenos <- paste(c("M_Age*Sex", "Height_med", paste("PC",as.character(1:10), sep = "")), collapse = " + ")
     formula_model0      <- paste(model_phenotype, model_covar_nogenos, sep = " ~ ")
     
-    model0              <- lm(formula_model0, data = input_data)
+    model0              <- lm(formula_model0, data = pheno_data)
     summary_m0          <- summary(model0)
     
-    input_data$Sample_Weight <- (input_data$N-1)*(input_data$S_Age^2) # Interaction of gender with age^2
-    dd              <- as.data.frame(cbind(input_data$Sample_Weight, 
-                                           input_data$Sample_Weight * (model0$residuals^2)))
+    pheno_data$Sample_Weight <- (pheno_data$N - 1)*(pheno_data$S_Age^2) # Interaction of gender with age^2
+    dd              <- as.data.frame(cbind(pheno_data$Sample_Weight, 
+                                           pheno_data$Sample_Weight * (model0$residuals^2)))
     colnames(dd)    <- c("w","y")
     model1          <- MASS::rlm(cbind(0*dd$w + 1,dd$w), dd$y)
     summary_m1      <- summary(model1)
     
-    input_data$Sample_Weight <- 1 / ( (1/input_data$Sample_Weight) + summary_m1$coefficients[2] / summary_m1$coefficients[1] )
+    pheno_data$Sample_Weight <- 1 / ( (1/pheno_data$Sample_Weight) + summary_m1$coefficients[2] / summary_m1$coefficients[1] )
     
     output$Variance_ratio <- as.numeric(model1$coefficients[2]/model1$coefficients[1])
     
     cat("> Estimating SNP effects...")
     
-    remove_cols <- c("IID", 
-                     "Weight_Beta", 
-                     "M_Age",
-                     "S_Age",
-                     "N",
-                     "Sample_Weight",
-                     "Sex", 
-                     "Height_med",
-                     paste0("PC", 1:10))
+    # Extract phenotype and covariates:
+    pheno_data$M_Age_Sex <- pheno_data$M_Age * pheno_data$Sex # Interaction term for Mid-age and gender
     
-    model_covar_genos <- colnames(input_data)[colnames(input_data) %ni% c(remove_cols)]
+    keep_cols_cols <- c("M_Age_Sex",
+                        "Height_med",
+                        paste0("PC", 1:10))
     
-    # Takes a few secs:
-    SNP_mapping    <- data.frame(Original_name = model_covar_genos,
-                                 Mapping       = paste0("SNP", 1:length(model_covar_genos)))
+    y          <- pheno_data$Weight_Beta
+    covariates <- as.matrix(pheno_data[, keep_cols_cols])
     
-    nonSNP_mapping <- data.frame(Original_name = remove_cols,
-                                 Mapping       = remove_cols)
+    ###########################
+    # Run per-SNP regression adjusting for age and gender
+    assoc_results <- bigstatsr::big_univLinReg(X      = geno_data, 
+                                               y, 
+                                               covar  = covariates#,
+                                               #ncores = numcores
+    )
     
-    all_mapping    <- rbind(nonSNP_mapping, SNP_mapping)
+    assoc_results$p.value   <- predict(assoc_results, log10 = FALSE)
+    assoc_results$score     <- NULL
+    colnames(assoc_results) <- c("Beta","SE","P")
     
-    colnames(input_data) <- all_mapping[match(colnames(input_data), all_mapping[,1]), 2]
+    #attr(assoc_results, "predict")
     
-    #colSums(input_data[c("SNP1", "SNP2", "SNP3", "SNP4")])
+    # dfs                     <- nrow(pheno_data) - length(keep_cols_cols) - 1 # n - k predictors - 1
+    # assoc_results$Pvalue2   <- pchisq((assoc_results[,1]/assoc_results[,2])^2, df=1, lower=F) # Source: https://www.mv.helsinki.fi/home/mjxpirin/GWAS_course/material/GWAS9.html
+    # assoc_results$Pvalue3 <- 2*stats::pt(assoc_results[,1]/assoc_results[,2], 
+    #                                    df         = dfs, 
+    #                                    lower.tail = FALSE, 
+    #                                    log.p      = FALSE) # Source: https://advstats.psychstat.org/book/mregression/index.php
+    # 
+    # head(assoc_results)
     
-    fitAllSNPs <-
-      pbapply::pblapply(SNP_mapping$Mapping[1:length(SNP_mapping$Mapping)], 
-                        function(SNP, 
-                                 input_data,
-                                 model_covar_nogenos,
-                                 model_phenotype){
-                          model1_covars     <- paste(model_covar_nogenos, SNP,       sep = " + ") 
-                          formula_model1    <- paste(model_phenotype, model1_covars, sep = " ~ ")
-                          
-                          fit <- lm(formula_model1,
-                                    data    = input_data,
-                                    weights = input_data$Sample_Weight
-                          )
-                          
-                          summary_m2    <- summary(fit)$coefficients
-                          extract_stats <- summary_m2[rownames(summary_m2)[rownames(summary_m2) %in% SNP],]
-                          
-                          # If the SNP for all samples is homozygous (2), or heterozygous (1), 
-                          # or there is no variant detected (0), there is no variability to be
-                          # modelled and the effect estimates are NA. Set them to -9999.
-                          tmp_names <- colnames(extract_stats)
-                          if( length(extract_stats) == 0)  extract_stats <- rbind(extract_stats,
-                                                                                  rep(-9999, 4))
-                          
-                          return(extract_stats)
-                        }, 
-                        input_data          = input_data,
-                        model_covar_nogenos = model_covar_nogenos,
-                        model_phenotype     = model_phenotype,
-                        cl = cl)
+    #---- Transform the linear regression coefs and SEs and then calculate the p-value using the X^2 distribution:
+    y_w          <- pheno_data$Weight_Beta * sqrt(pheno_data$Sample_Weight)
+    covariates_w <- sweep(pheno_data[, keep_cols_cols], 1, sqrt(pheno_data$Sample_Weight), "*")
+    covariates_w <- as.matrix(covariates_w)
     
-    out           <- data.frame(do.call(rbind, fitAllSNPs))
-    out$"t.value" <- NULL
+    geno_data_w <- geno_data$bm()
+    #class(geno_data_w)
+    #head(geno_data_w[1:3,1:3])
+    
+    # Source: https://privefl.github.io/bigstatsr/articles/bigstatsr-and-bigmemory.html
+    #geno_data_w <- BM2FBM( sweep(geno_data_w[,1:ncol(geno_data_w)], 1, sqrt(pheno_data$Sample_Weight), "*") )
+    #class(geno_data_w)
+    geno_data_w <- BM2FBM(geno_data_w)
+    #class(geno_data_w)
+    
+    out <- bigstatsr::big_univLinReg(X      = geno_data_w, 
+                                     y_w, 
+                                     covar  = covariates_w#,
+                                     #ncores = numcores
+    )
+    
+    out$p.value <- predict(out, log10 = FALSE)
+    out$score <- NULL
     colnames(out) <- c("Beta","SE","P")
     
-    # effect_size_output$AF <- mean(input_data$geno)/2 # Anders should explain the rationale for calculating the Allelic Frequency
+    rm(assoc_results)
+    # head(out)
     
-    out                 <- cbind(SNP_mapping$Original_name, out)
-    colnames(out)[1]    <- "SNP"
-    output$Effect_sizes <- out
+    output$Effect_sizes <- cbind(SNP_IDs, out)
     
   }# End if
   
   # Output:
   
   return(output)
-  
-  stopCluster(cl)
   
 }# End function
